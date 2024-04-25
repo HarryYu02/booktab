895dfcb82d30e89a2e4d35416c573847cf30a86e
Harry Yu <yupakhei456@gmail.com>
2024-04-25 04:56:45 -0400

    extract the CommandActions component, added popover to it

--- README.md
+++ README.md
@@ -10,33 +10,33 @@ When I was doing some coding, I have many documentation opened and also some dee
 
 There are also some work around like pressing Ctrl+Shift+A to search tabs, or press F6 and type @bookmark or @tabs to perform a search, but I am too lazy for that many buttons or mouse clicks, so I spent 2 days to automate a task that would have took me 10 seconds instead.
 
-My usecase for BookTab is to run it using a Powershell Script to run it, save all the code documentations in a single folder so it doesnt clutter up my bookmark bar, and search for them when I need them.
+My usecase for BookTab is to save all the code documentations in a single folder so it doesnt clutter up my bookmark bar, and search for them when I need them.
 
 ## Features
 
--   Searching through bookmarks and tabs
--   Open bookmarks on new tab
--   Navigate to specific tab
--   Search the text on google
--   Light/Dark theme
+- Searching through bookmarks and tabs
+- Open bookmarks on new tab
+- Navigate to specific tab
+- Search the text on google
+- Light/Dark theme
 
 ## Technologies Used
 
--   [React](https://react.dev/)
--   [TypeScript](https://www.typescriptlang.org/)
--   [Vite](https://vitejs.dev/) (Build tool)
--   [Chrome Extension Manifest V3](https://developer.chrome.com/docs/extensions)
--   [CRXJS/vite-plugin](https://crxjs.dev/vite-plugin) (Support for Chrome Extension API)
--   [TailwindCSS](https://tailwindcss.com/)
--   [ShadcnUI](https://ui.shadcn.com/)
+- [React](https://react.dev/)
+- [TypeScript](https://www.typescriptlang.org/)
+- [Vite](https://vitejs.dev/) (Build tool)
+- [Chrome Extension Manifest V3](https://developer.chrome.com/docs/extensions)
+- [CRXJS/vite-plugin](https://crxjs.dev/vite-plugin) (Support for Chrome Extension API)
+- [TailwindCSS](https://tailwindcss.com/)
+- [ShadcnUI](https://ui.shadcn.com/)
 
 ## Installation and Usage
 
 Before running this project locally, ensure you have the following installed on your system:
 
--   [Node.js](https://nodejs.org/) (v20.x or later)
--   [Yarn](https://yarnpkg.com/) (you can use npm but I use yarn for this project)
--   [Git](https://git-scm.com/)
+- [Node.js](https://nodejs.org/) (v20.x or later)
+- [Yarn](https://yarnpkg.com/) (you can use npm but I use yarn for this project)
+- [Git](https://git-scm.com/)
 
 To try out BookTab locally:
 
@@ -122,21 +122,21 @@ That's it! Your BookTab extension should now be updated with the latest changes.
 
 ## Keyboard Shortcuts
 
-BookTab supports the following keyboard shortcuts:
+BookTab supports the following keyboard shortcuts (basically all shortcuts that's compatible with cmdk):
 
--   **Open / Close Extension**: Ctrl+Shift+P (Mac: Command+Shift+P)
--   **Move to Next Option**: Down / Ctrl+N / Ctrl+J
--   **Move to Previous Option**: Up / Ctrl+P / Ctrl+K
--   **Select Current Option**: Enter
+- **Open / Close Extension**: Ctrl+Shift+P (Mac: Command+Shift+P)
+- **Move to Next Option**: Down / Ctrl+N / Ctrl+J
+- **Move to Previous Option**: Up / Ctrl+P / Ctrl+K
+- **Select Current Option**: Enter
 
-I have customize it so I can use left hand only, but you can customize them in manifest,json, while users can customize these shortcuts under `chrome://extensions/` > Keyboard Shortcut in the sidebar.
+You can customize the open/close extension shortcut in manifest,json, while users can customize these shortcuts under `chrome://extensions/` > Keyboard Shortcut in the sidebar.
 
 ## Contributions
 
 Contributions to BookTab are welcome! Here are a few ways you can contribute:
 
--   Fork the repository and submit a pull request.
--   Open an issue to report bugs or suggest improvements.
+- Fork the repository and submit a pull request.
+- Open an issue to report bugs or suggest improvements.
 
 ## License
 
--- src/components/SearchingList.tsx
+++ src/components/SearchingList.tsx
@@ -1,10 +1,10 @@
 import {
-    Command,
-    CommandEmpty,
-    CommandGroup,
-    CommandInput,
-    CommandList,
-    CommandShortcut,
+  Command,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandList,
+  CommandShortcut,
 } from "@/components/ui/command";
 import SearchListItem from "./SearchListItem";
 import { useEffect, useRef, useState } from "react";
@@ -13,81 +13,75 @@ import ThemeListItem from "./ThemeListItem";
 import TabList from "./TabList";
 import BookmarkList from "./BookmarkList";
 import { IoIosReturnLeft } from "react-icons/io";
-import { ImCtrl } from "react-icons/im";
 import { Separator } from "./ui/separator";
 import useTabs from "@/hooks/useTabs";
 import useBookmarks from "@/hooks/useBookmarks";
+import CommandActions from "./CommandActions";
 
 const SearchingList = () => {
-    const tabData = useTabs();
-    const bookmarkData = useBookmarks();
-    const [searchText, setSearchText] = useState("");
+  const [currentItem, setCurrentItem] = useState("");
+  const [searchText, setSearchText] = useState("");
+  const [openActions, setOpenActions] = useState(false);
+  const tabData = useTabs();
+  const bookmarkData = useBookmarks();
+  const listRef = useRef<HTMLDivElement | null>(null);
 
-    const listRef = useRef<HTMLDivElement | null>(null);
+  useEffect(() => {
+    const commandList = listRef.current;
+    // console.log(commandList);
+    commandList?.scrollTo(0, 0);
+  }, [searchText]);
 
-    useEffect(() => {
-        const commandList = listRef.current;
-        console.log(commandList);
-        commandList?.scrollTo(0, 0);
-    }, [searchText]);
-
-    return (
-        <Command
-            className="flex flex-col rounded-lg border shadow-md"
-            label="BookTab"
+  return (
+    <Command
+      className="flex flex-col rounded-lg border shadow-md"
+      label="BookTab"
+      value={currentItem}
+      onValueChange={setCurrentItem}
+    >
+      <CommandInput
+        autoFocus
+        className="h-12"
+        placeholder="Search for tabs and bookmarks..."
+        value={searchText}
+        onValueChange={(value) => {
+          setSearchText(value);
+        }}
+      />
+      <CommandList
+        ref={listRef}
+        className="h-full max-h-full [overflow-anchor:auto]"
+        defaultValue={""}
+      >
+        <CommandEmpty>No results found</CommandEmpty>
+        <CommandGroup heading="Result">
+          <TabList tabData={tabData} />
+          <BookmarkList bookmarkData={bookmarkData} />
+          <ThemeListItem key="dark-theme" targetTheme="dark" />
+          <ThemeListItem key="light-theme" targetTheme="light" />
+          <ThemeListItem key="system-theme" targetTheme="system" />
+          <SearchListItem key="search-google" searchText={searchText} />
+        </CommandGroup>
+      </CommandList>
+      <div className="flex items-center justify-end border-t p-1">
+        <Button
+          className="flex items-center gap-2 px-2 text-base hover:cursor-default"
+          variant={"ghost"}
         >
-            <CommandInput
-                autoFocus
-                className="h-12"
-                placeholder="Search for tabs and bookmarks..."
-                value={searchText}
-                onValueChange={(value) => {
-                    setSearchText(value);
-                }}
-            />
-            <CommandList
-                ref={listRef}
-                className="h-full max-h-full [overflow-anchor:auto]"
-            >
-                <CommandEmpty>No results found</CommandEmpty>
-                <CommandGroup heading="Result">
-                    <TabList tabData={tabData} />
-                    <BookmarkList bookmarkData={bookmarkData} />
-                    <ThemeListItem key="dark-theme" targetTheme="dark" />
-                    <ThemeListItem key="light-theme" targetTheme="light" />
-                    <ThemeListItem key="system-theme" targetTheme="system" />
-                    <SearchListItem
-                        key="search-google"
-                        searchText={searchText}
-                    />
-                </CommandGroup>
-            </CommandList>
-            <div className="flex items-center justify-end border-t p-1">
-                <Button
-                    className="flex items-center gap-2 px-2 text-base hover:cursor-default"
-                    variant={"ghost"}
-                >
-                    Open
-                    <CommandShortcut className="size-8 rounded-lg bg-secondary p-2">
-                        <IoIosReturnLeft className="size-4" />
-                    </CommandShortcut>
-                </Button>
-                <Separator orientation="vertical" className="m-2 h-3/4" />
-                <Button
-                    className="flex items-center gap-2 px-2 text-base text-muted-foreground hover:cursor-default"
-                    variant={"ghost"}
-                >
-                    Actions
-                    <CommandShortcut className="size-8 rounded-lg bg-secondary p-2">
-                        <ImCtrl className="size-4" />
-                    </CommandShortcut>
-                    <CommandShortcut className="size-8 rounded-lg bg-secondary p-2">
-                        <span className="size-4">K</span>
-                    </CommandShortcut>
-                </Button>
-            </div>
-        </Command>
-    );
+          Open
+          <CommandShortcut className="size-8 rounded-lg bg-secondary p-2">
+            <IoIosReturnLeft className="size-4" />
+          </CommandShortcut>
+        </Button>
+        <Separator orientation="vertical" className="m-2 h-3/4" />
+        <CommandActions
+          currentItem={currentItem}
+          openActions={openActions}
+          setOpenActions={setOpenActions}
+        />
+      </div>
+    </Command>
+  );
 };
 
 export default SearchingList;
