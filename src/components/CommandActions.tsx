6799abc97b8b938955cb03ac516fc8319cf74baf
Harry Yu <yupakhei456@gmail.com>
2024-04-25 20:48:15 -0400

    pass in item that correspond to the selected item to command action

--- src/components/CommandActions.tsx
+++ src/components/CommandActions.tsx
@@ -1,48 +1,120 @@
 import { Button } from "./ui/button";
-import { CommandShortcut } from "./ui/command";
 import {
-  Popover,
-  PopoverContent,
-  PopoverTrigger,
+    Command,
+    CommandEmpty,
+    CommandGroup,
+    CommandInput,
+    CommandItem,
+    CommandList,
+    CommandShortcut,
+} from "./ui/command";
+import {
+    Popover,
+    PopoverContent,
+    PopoverTrigger,
 } from "@/components/ui/popover";
 import React from "react";
 
+type TabTarget = {
+    itemType: "tab";
+    target: chrome.tabs.Tab;
+};
+
+type BookmarkTarget = {
+    itemType: "bookmark";
+    target: chrome.bookmarks.BookmarkTreeNode;
+};
+
+type CmdTarget = {
+    itemType: "command";
+    target: string;
+};
+
+type Target = TabTarget | BookmarkTarget | CmdTarget;
+
 const CommandActions = ({
-  currentItem,
-  openActions,
-  setOpenActions,
-  command,
-}: {
-  currentItem: string;
-  openActions: boolean;
-  setOpenActions: React.Dispatch<React.SetStateAction<boolean>>;
-  command: chrome.commands.Command | null;
+    itemType,
+    itemName,
+    openActions,
+    setOpenActions,
+    command,
+    target,
+}: Target & {
+    itemName: string;
+    openActions: boolean;
+    setOpenActions: React.Dispatch<React.SetStateAction<boolean>>;
+    command: chrome.commands.Command | null;
 }) => {
-  return (
-    <Popover open={openActions} onOpenChange={setOpenActions}>
-      <PopoverTrigger asChild>
-        <Button
-          className="flex items-center gap-2 px-2 text-base text-muted-foreground hover:cursor-default"
-          variant={"ghost"}
-        >
-          Actions
-          {command?.shortcut?.split("").map((key) => (
-            <CommandShortcut
-              key={`command_actions-key-${key}`}
-              className="size-8 rounded-lg bg-secondary p-2"
-            >
-              <span className="size-4">{key}</span>
-            </CommandShortcut>
-          ))}
-        </Button>
-      </PopoverTrigger>
-      <PopoverContent>
-        {currentItem.slice(0, currentItem.indexOf("-"))}
-        <br />
-        {currentItem.slice(currentItem.indexOf("-") + 1)}
-      </PopoverContent>
-    </Popover>
-  );
+    return (
+        <Popover open={openActions} onOpenChange={setOpenActions}>
+            <PopoverTrigger asChild>
+                <Button
+                    className="flex items-center gap-2 px-2 text-base text-muted-foreground hover:cursor-default"
+                    variant={"ghost"}
+                >
+                    Actions
+                    {command?.shortcut?.split("").map((key) => (
+                        <CommandShortcut
+                            key={`command_actions-key-${key}`}
+                            className="size-8 rounded-lg bg-secondary p-2"
+                        >
+                            <span className="size-4">{key}</span>
+                        </CommandShortcut>
+                    ))}
+                    {(!command || !command.shortcut) && " Not Set"}
+                </Button>
+            </PopoverTrigger>
+            <PopoverContent>
+                <Command className="">
+                    <p className="text-xs text-muted-foreground">{itemType}</p>
+                    <p className="w-full truncate text-base">{itemName}</p>
+                    <CommandList>
+                        <CommandGroup heading={"Actions"} forceMount>
+                            {itemType === "tab" ? (
+                                <>
+                                    <CommandItem
+                                        onSelect={() => {
+                                            chrome.tabs
+                                                .remove(
+                                                    target.id ??
+                                                        chrome.tabs.TAB_ID_NONE
+                                                )
+                                                .catch((error) => {
+                                                    console.log(error);
+                                                });
+                                            window.close();
+                                        }}
+                                    >
+                                        Close Tab
+                                    </CommandItem>
+                                    <CommandItem
+                                        onSelect={() => {
+                                            chrome.tabs
+                                                .duplicate(
+                                                    target.id ??
+                                                        chrome.tabs.TAB_ID_NONE
+                                                )
+                                                .catch((error) => {
+                                                    console.log(error);
+                                                });
+                                            window.close();
+                                        }}
+                                    >
+                                        Copy Tab
+                                    </CommandItem>
+                                </>
+                            ) : itemType === "bookmark" ? (
+                                <></>
+                            ) : itemType === "command" ? (
+                                <></>
+                            ) : null}
+                        </CommandGroup>
+                    </CommandList>
+                    <CommandInput />
+                </Command>
+            </PopoverContent>
+        </Popover>
+    );
 };
 
 export default CommandActions;
--- src/components/SearchingList.tsx
+++ src/components/SearchingList.tsx
@@ -1,10 +1,10 @@
 import {
-  Command,
-  CommandEmpty,
-  CommandGroup,
-  CommandInput,
-  CommandList,
-  CommandShortcut,
+    Command,
+    CommandEmpty,
+    CommandGroup,
+    CommandInput,
+    CommandList,
+    CommandShortcut,
 } from "@/components/ui/command";
 import SearchListItem from "./SearchListItem";
 import { useEffect, useRef, useState } from "react";
@@ -18,98 +18,118 @@ import useTabs from "@/hooks/useTabs";
 import useBookmarks from "@/hooks/useBookmarks";
 import CommandActions from "./CommandActions";
 
+type ItemType = "tab" | "bookmark" | "command";
+
 const SearchingList = () => {
-  const [currentItem, setCurrentItem] = useState("");
-  const [searchText, setSearchText] = useState("");
-  const [openActions, setOpenActions] = useState(false);
-  const [commands, setCommands] = useState<chrome.commands.Command[]>([]);
-  const tabData = useTabs();
-  const bookmarkData = useBookmarks();
-  const listRef = useRef<HTMLDivElement | null>(null);
+    const [currentItem, setCurrentItem] = useState("");
+    const [searchText, setSearchText] = useState("");
+    const [openActions, setOpenActions] = useState(false);
+    const [commands, setCommands] = useState<chrome.commands.Command[]>([]);
+    const tabData = useTabs();
+    const bookmarkData = useBookmarks();
+    const listRef = useRef<HTMLDivElement | null>(null);
 
-  useEffect(() => {
-    const onCommandHandler = (command: string) => {
-      console.log(`Command "${command}" triggered`);
-      if (command === "command_actions") {
-        setOpenActions((prev) => !prev);
-      }
-    };
+    useEffect(() => {
+        const onCommandHandler = (command: string) => {
+            console.log(`Command "${command}" triggered`);
+            if (command === "command_actions") {
+                setOpenActions((prev) => !prev);
+            }
+        };
 
-    const getCommandShortcuts = async () => {
-      const defaultCommands = await chrome.commands.getAll();
-      setCommands(defaultCommands);
-      return defaultCommands;
-    };
-    getCommandShortcuts().catch((error) => {
-      console.log(error);
-    });
+        const getCommandShortcuts = async () => {
+            const defaultCommands = await chrome.commands.getAll();
+            setCommands(defaultCommands);
+            return defaultCommands;
+        };
+        getCommandShortcuts().catch((error) => {
+            console.log(error);
+        });
 
-    chrome.commands.onCommand.addListener(onCommandHandler);
-    return () => {
-      setOpenActions(false);
-      chrome.commands.onCommand.removeListener(onCommandHandler);
-    };
-  }, []);
+        chrome.commands.onCommand.addListener(onCommandHandler);
+        return () => {
+            setOpenActions(false);
+            chrome.commands.onCommand.removeListener(onCommandHandler);
+        };
+    }, []);
 
-  useEffect(() => {
-    const commandList = listRef.current;
-    commandList?.scrollTo(0, 0);
-  }, [searchText]);
+    useEffect(() => {
+        const commandList = listRef.current;
+        commandList?.scrollTo(0, 0);
+    }, [searchText]);
 
-  return (
-    <Command
-      className="flex flex-col rounded-lg border shadow-md"
-      label="BookTab"
-      value={currentItem}
-      onValueChange={setCurrentItem}
-    >
-      <CommandInput
-        autoFocus
-        className="h-12"
-        placeholder="Search for tabs and bookmarks..."
-        value={searchText}
-        onValueChange={(value) => {
-          setSearchText(value);
-        }}
-      />
-      <CommandList
-        ref={listRef}
-        className="h-full max-h-full [overflow-anchor:auto]"
-        defaultValue={""}
-      >
-        <CommandEmpty>No results found</CommandEmpty>
-        <CommandGroup heading="Result">
-          <TabList tabData={tabData} />
-          <BookmarkList bookmarkData={bookmarkData} />
-          <ThemeListItem key="dark-theme" targetTheme="dark" />
-          <ThemeListItem key="light-theme" targetTheme="light" />
-          <ThemeListItem key="system-theme" targetTheme="system" />
-          <SearchListItem key="search-google" searchText={searchText} />
-        </CommandGroup>
-      </CommandList>
-      <div className="flex items-center justify-end border-t p-1">
-        <Button
-          className="flex items-center gap-2 px-2 text-base hover:cursor-default"
-          variant={"ghost"}
+    const item = currentItem.split("-");
+    const itemType = item[0] as ItemType;
+    const itemValue = item[1];
+
+    return (
+        <Command
+            className="flex flex-col rounded-lg border shadow-md"
+            label="BookTab"
+            value={currentItem}
+            onValueChange={setCurrentItem}
         >
-          Open
-          <CommandShortcut className="size-8 rounded-lg bg-secondary p-2">
-            <IoIosReturnLeft className="size-4" />
-          </CommandShortcut>
-        </Button>
-        <Separator orientation="vertical" className="m-2 h-3/4" />
-        <CommandActions
-          currentItem={currentItem}
-          openActions={openActions}
-          setOpenActions={setOpenActions}
-          command={
-            commands.find((command) => command.name === "command_actions") ??
-            null
-          }
-        />
-      </div>
-    </Command>
-  );
+            <CommandInput
+                autoFocus
+                className="h-12"
+                placeholder="Search for tabs and bookmarks..."
+                value={searchText}
+                onValueChange={(value) => {
+                    setSearchText(value);
+                }}
+            />
+            <CommandList
+                ref={listRef}
+                className="h-full max-h-full [overflow-anchor:auto]"
+                defaultValue={""}
+            >
+                <CommandEmpty>No results found</CommandEmpty>
+                <CommandGroup heading="Result">
+                    <TabList tabData={tabData} />
+                    <BookmarkList bookmarkData={bookmarkData} />
+                    <ThemeListItem key="dark-theme" targetTheme="dark" />
+                    <ThemeListItem key="light-theme" targetTheme="light" />
+                    <ThemeListItem key="system-theme" targetTheme="system" />
+                    <SearchListItem
+                        key="search-google"
+                        searchText={searchText}
+                    />
+                </CommandGroup>
+            </CommandList>
+            <div className="flex items-center justify-end border-t p-1">
+                <Button
+                    className="flex items-center gap-2 px-2 text-base hover:cursor-default"
+                    variant={"ghost"}
+                >
+                    Open
+                    <CommandShortcut className="size-8 rounded-lg bg-secondary p-2">
+                        <IoIosReturnLeft className="size-4" />
+                    </CommandShortcut>
+                </Button>
+                <Separator orientation="vertical" className="m-2 h-3/4" />
+                <CommandActions
+                    itemType={itemType}
+                    itemName={itemValue}
+                    openActions={openActions}
+                    setOpenActions={setOpenActions}
+                    command={
+                        commands.find(
+                            (command) => command.name === "command_actions"
+                        ) ?? null
+                    }
+                    target={
+                        (itemType === "tab"
+                            ? tabData.find((tab) => tab.title === itemValue)
+                            : itemType === "bookmark"
+                              ? bookmarkData.find(
+                                    (bookmark) => bookmark.title === itemValue
+                                )
+                              : "command") || "unknown"
+                    }
+                />
+            </div>
+        </Command>
+    );
 };
 
 export default SearchingList;
